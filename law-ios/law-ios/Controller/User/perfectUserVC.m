6dcaa5a4bdc471ddc8569aedb10e70c5b53eb009
李雪阳 <906099632@qq.com>
2018-02-28 22:38:03 +0800

    修改bug

--- law-ios/law-ios/Controller/User/perfectUserVC.m
+++ law-ios/law-ios/Controller/User/perfectUserVC.m
@@ -74,6 +74,9 @@
 
 -(void)bindView {
     self.title = @"补充个人信息";
+    if (self.un){
+        self.title = @"修改个人信息";
+    }
     self.view.backgroundColor = [UIColor groupTableViewBackgroundColor];
     self.tvList = [[UITableView alloc]initWithFrame:CGRectMake(0, 0,WIDTH_ ,HEIGHT_ - 150) style:UITableViewStyleGrouped];
     if (self.uc) {
@@ -93,7 +96,7 @@
     if (self.un) {
         self.btnRight.hidden = NO;
         [self.btnRight setTitleColor:BLUE forState:UIControlStateNormal];
-        [self.btnRight setTitle:@"更新" forState:UIControlStateNormal];
+        [self.btnRight setTitle:@"保存" forState:UIControlStateNormal];
     }
 }
 
@@ -130,21 +133,28 @@
     [mdict setValue:@1 forKey:@"userType"];
     [mdict setValue:@1 forKey:@"licenseType"];
     [mdict setValue:[[[NSUserDefaults standardUserDefaults] valueForKey:[NSString stringWithFormat:@"companyList%@",phone]] valueForKey:@"name"] forKey:@"commpanyName"];
-    [self POSTurl:REGIST parameters:@{@"data":[self dictionaryToJson:mdict]} success:^(id responseObject) {
-        if ([responseObject[@"messageCode"] intValue] == 10000) {
-            if (self.un) {
-                [[Toast shareToast]makeText:@"更新信息" aDuration:1];
+    if ([self judgeIdentityStringValid:self.UserCardNum.text]) {
+        [self POSTurl:REGIST parameters:@{@"data":[self dictionaryToJson:mdict]} success:^(id responseObject) {
+            if ([responseObject[@"messageCode"] intValue] == 10000) {
+                if (self.un) {
+                    [[Toast shareToast]makeText:@"保存成功" aDuration:1];
+                }
+                
+                [self onBackAction];
+            }else{
+                [[Toast shareToast]makeText:@"信息不完整" aDuration:1];
             }
-            
-            [self onBackAction];
-        }else{
-            [[Toast shareToast]makeText:@"信息不完整" aDuration:1];
-        }
+            [SVProgressHUD dismiss];
+        } failure:^(id responseObject) {
+            [[Toast shareToast]makeText:@"服务繁忙" aDuration:1];
+            [SVProgressHUD dismiss];
+        }];
+    }else{
+        [[Toast shareToast]makeText:@"身份证号不正确" aDuration:1];
         [SVProgressHUD dismiss];
-    } failure:^(id responseObject) {
-        [[Toast shareToast]makeText:@"服务繁忙" aDuration:1];
-        [SVProgressHUD dismiss];
-    }];
+    }
+    
+    
 }
 
 /**
@@ -524,4 +534,50 @@
         faile(error);
     }];
 }
+
+- (BOOL)judgeIdentityStringValid:(NSString *)identityString {
+    
+    if (identityString.length != 18) return NO;
+    // 正则表达式判断基本 身份证号是否满足格式
+    NSString *regex = @"^[1-9]\\d{5}[1-9]\\d{3}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}([0-9]|X)$";
+    //  NSString *regex = @"^(^[1-9]\\d{7}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])\\d{3}$)|(^[1-9]\\d{5}[1-9]\\d{3}((0\\d)|(1[0-2]))(([0|1|2]\\d)|3[0-1])((\\d{4})|\\d{3}[Xx])$)$";
+    NSPredicate *identityStringPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex];
+    //如果通过该验证，说明身份证格式正确，但准确性还需计算
+    if(![identityStringPredicate evaluateWithObject:identityString]) return NO;
+    
+    //** 开始进行校验 *//
+    
+    //将前17位加权因子保存在数组里
+    NSArray *idCardWiArray = @[@"7", @"9", @"10", @"5", @"8", @"4", @"2", @"1", @"6", @"3", @"7", @"9", @"10", @"5", @"8", @"4", @"2"];
+    
+    //这是除以11后，可能产生的11位余数、验证码，也保存成数组
+    NSArray *idCardYArray = @[@"1", @"0", @"10", @"9", @"8", @"7", @"6", @"5", @"4", @"3", @"2"];
+    
+    //用来保存前17位各自乖以加权因子后的总和
+    NSInteger idCardWiSum = 0;
+    for(int i = 0;i < 17;i++) {
+        NSInteger subStrIndex = [[identityString substringWithRange:NSMakeRange(i, 1)] integerValue];
+        NSInteger idCardWiIndex = [[idCardWiArray objectAtIndex:i] integerValue];
+        idCardWiSum+= subStrIndex * idCardWiIndex;
+    }
+    
+    //计算出校验码所在数组的位置
+    NSInteger idCardMod=idCardWiSum%11;
+    //得到最后一位身份证号码
+    NSString *idCardLast= [identityString substringWithRange:NSMakeRange(17, 1)];
+    //如果等于2，则说明校验码是10，身份证号码最后一位应该是X
+    if(idCardMod==2) {
+        if(![idCardLast isEqualToString:@"X"]|| ![idCardLast isEqualToString:@"x"]) {
+            return NO;
+        }
+    }
+    else{
+        //用计算出的验证码与最后一位身份证号码匹配，如果一致，说明通过，否则是无效的身份证号码
+        if(![idCardLast isEqualToString: [idCardYArray objectAtIndex:idCardMod]]) {
+            return NO;
+        }
+    }
+    return YES;
+}
+
 @end
